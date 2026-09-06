f9fd30a9f473841dcd4c79d59e5a197ca80bb8fb
kirilsfriends-glitch <kirilsfriends@gmail.com>
2026-09-06 12:42:13 +0300

    Add GameThread

---
+++ src/main/java/com/flappybird/game/GameThread.java
@@ -0,0 +1,42 @@
+package com.flappybird.game;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private boolean running = false;
+
+    public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            Canvas canvas = null;
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    gameView.update();
+                    gameView.draw(canvas);
+                }
+            } finally {
+                if (canvas != null) {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+            try {
+                Thread.sleep(16);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
